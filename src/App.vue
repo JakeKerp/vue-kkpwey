8b6db4bd55a7ab5a180772e9ae85a9821495f3ca
JakeKerp <98071085+JakeKerp@users.noreply.github.com>
2023-04-12 23:24:51 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <div class="row">
+      <Header :varFromParent="var01" @newList="updateList($event)" />
+    </div>
+    <div class="row">
+      <TableHeadings />
+    </div>
+    <div class="row">
+      <DataTable
+        :varFromParent="var01"
+        @newList="updateList($event)"
+        style="position: relative; width: 100%"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import TableHeadings from './components/TableHeadings.vue';
+import DataTable from './components/DataTable.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    TableHeadings,
+    DataTable,
+  },
+  methods: {
+    updateList(varFromChild) {
+      this.var01 = varFromChild.data;
+    },
+  },
+  data: () => ({
+    var01: [
+      ['title01', 'description01', '2022-11-03', 'med', false],
+      ['title02', 'description02', '2022-11-03', 'med', true],
+    ],
+  }),
+};
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+button {
+  width: 100px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card text-white bg-primary" id="banner">
+    <div class="card-body">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col">
+          <i class="fa fa-bars" style="font-size: 18px"></i>
+          FRAMEWORKS
+        </div>
+        <div class="col">
+          <AddUpdate :varFromHeader="[varFromParent, true]" ; />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import AddUpdate from './AddUpdate.vue';
+export default {
+  name: 'Header',
+  components: {
+    AddUpdate,
+  },
+  props: ['varFromParent'],
+  data: () => ({}),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#banner {
+  position: relative;
+  background-color: #1666bf;
+  top: 0;
+}
+</style>
